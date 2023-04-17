0daaecd6c18a94c5769f09c283306de28ebfb475
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-04-17 22:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
+   let decodedarr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0 ; i<encodedStr.length ; i++)
 		{
